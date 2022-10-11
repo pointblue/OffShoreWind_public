6883ea28446f5faa750532fdb94cf1031b02604d
Cotton Rockwood <crockwood@pointblue.org>
2022-10-10 17:14:08 -0700

    Adding to Fargate set-up

--- scripts/Fargate_Set-up.R
+++ scripts/Fargate_Set-up.R
@@ -24,20 +24,32 @@ lapply(libs, require, character.only = TRUE)
 #--------------------------------------------------------------------------
 # Fargate set-up and registry
 #--------------------------------------------------------------------------
-# Initiate Elastic Container Registry (ECR) service
+# Initiate Elastic Container Registry (ECR) connection
+# credentials and region should already be configured in the AWS CLI
 ecr_svc <- ecr()
 
 #--------------------------------------------------------------------------
-# Create AWS ECRrepository
-
+# Create AWS ECR repository
+ecr_svc$create_repository(repositoryName = "")
 
 #--------------------------------------------------------------------------
 # Tag Docker image
-
+ecr_svc$tag_resource()
 
 #--------------------------------------------------------------------------
 # Get ECR keys
-
+ecr_svc$get_authorization_token()
 
 #--------------------------------------------------------------------------
 # Push image to ECR
+ecr_svc$initiate_layer_upload()
+
+#--------------------------------------------------------------------------
+# Fargate Task registry
+#--------------------------------------------------------------------------
+# Initiate Elastic Container Service (ECS) connection
+ecs_svc <- ecs()
+
+#--------------------------------------------------------------------------
+# Create compute cluster
+ecs_svc$
